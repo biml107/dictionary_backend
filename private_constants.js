b03a003d23950e064259ccfcf57652797fc87329
Bimal <biml20000@gmail.com>
2024-07-06 22:52:50 +0530

    version 1

---
+++ private_constants.js
@@ -0,0 +1,7 @@
+const privateConstants = {};
+
+privateConstants.bmlkumar2000Project0URI = `mongodb+srv://bmlkumar2000:brother@cluster0.b0wedxn.mongodb.net/twitter?retryWrites=true&w=majority`
+privateConstants.SESSIONKEY=`Helloworld1`;
+privateConstants.LOCAL_MONGODB_URI = 'mongodb://localhost:27017';
+
+export { privateConstants as default};
